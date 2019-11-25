7433a5f71a7a9cbefb5c52a8c69d688f70679ccd
IlyaKot <50303550+ilyak93@users.noreply.github.com>
2019-11-25 18:35:30 +0200

    added probability to EventType

--- sase/base/EventType.java
+++ sase/base/EventType.java
@@ -1,15 +1,16 @@
 package sase.base;
 
+import sase.simulator.Environment;
+
 import java.util.ArrayList;
 import java.util.List;
-
-import sase.simulator.Environment;
 
 public class EventType {
 
 	private final String name;
 	private final List<Attribute> attributes;
-	
+	private static double prob;
+
 	public EventType(String name, Attribute[] attributes) {
 		this.name = name;
 		this.attributes = new ArrayList<Attribute>();
@@ -28,6 +29,14 @@ public class EventType {
 	
 	public Double getRate() {
 		return Environment.getEnvironment().getEventRateEstimator().getEventRateEstimate(this);
+	}
+
+	public double getProb() {
+		return this.prob;
+	}
+
+	public void setProb(double prob) {
+		this.prob = prob;
 	}
 	
 	@Override
