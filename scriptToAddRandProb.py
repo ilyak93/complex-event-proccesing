435e3f5c414858e1a9cb9c53f6edae9778bf052e
idanmarton <57764203+idanmarton@users.noreply.github.com>
2020-04-02 20:03:14 +0300

    Delete scriptToAddRandProb.py

--- scriptToAddRandProb.py
+++
@@ -1,8 +0,0 @@
-import random
-
-filepath = "NASDAQ_20080204_1.txt"
-with open(filepath, 'r') as f:     # load file
-    lines = f.read().splitlines()  # read lines
-
-with open('new_NASDAQ_20080204_1.txt', 'w') as f:
-    f.write('\n'.join([line+',Prob:'+(random.randint(0, 100)/100.0).__str__() for line in lines]))  # write lines with '#' appended
