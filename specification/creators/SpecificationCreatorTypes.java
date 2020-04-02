489276a7a9de26158ec5415eeac04bbfdaaa0926
IlyaKot <50303550+ilyak93@users.noreply.github.com>
2020-04-02 21:42:15 +0300

    final v.2

---
+++ specification/creators/SpecificationCreatorTypes.java
@@ -0,0 +1,10 @@
+package sase.specification.creators;
+
+public enum SpecificationCreatorTypes {
+	NONE,
+	CORRELATION_SELECTIVITY_ESTIMATION,
+	DELTA_SELECTIVITY_ESTIMATION,
+	CROSS_PRODUCT_SINGLE,
+	STOCK_EVALUATION,
+	CROSS_PRODUCT_MULTI_STOCK,
+}
