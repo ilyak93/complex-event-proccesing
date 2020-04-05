26b0bf76cf37b46ad7ba16b0b211b1ba77d26f9d
IlyaKot <50303550+ilyak93@users.noreply.github.com>
2020-04-05 23:08:20 +0300

    relative path fix

--- sase/config/path/WindowsPathConfig.java
+++ sase/config/path/WindowsPathConfig.java
@@ -3,12 +3,12 @@ package sase.config.path;
 public class WindowsPathConfig extends PathConfig {
 
 	public WindowsPathConfig() {
-		companyToRegionDirectoryPath = "Regions";
-		testInputFilePath = "testInputFile200";
-		firstInputFilePath = "NASDAQ_20080204_1.txt";
-		firstInputDirectoryPath = "input_dir_path";
-		outputFilePath = "output.csv";
-		outputFilePathProb = "outputProb.csv";
-		selectivityEstimatorsFilePath = "selectivity.ser";
+		companyToRegionDirectoryPath = ".\\Regions";
+		testInputFilePath = ".\\testInputFile200";
+		firstInputFilePath = ".\\NASDAQ_20080204_1.txt";
+		firstInputDirectoryPath = ".\\input_dir_path";
+		outputFilePath = ".\\output.csv";
+		outputFilePathProb = ".\\outputProb.csv";
+		selectivityEstimatorsFilePath = ".\\selectivity.ser";
 	}
 }
